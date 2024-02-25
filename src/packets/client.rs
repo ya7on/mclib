353123e3e37fdf56aceb5ed677edc9760941d7df
ya7on <hello@syoma.website>
2024-02-25 16:14:21 +0300

    Sort packet exports

--- src/packets/client.rs
+++ src/packets/client.rs
@@ -1,20 +1,21 @@
 //! Packets that bound to client (from server to client)
 
 mod chunk_data_and_update_light;
-pub use chunk_data_and_update_light::{BlockEntity, ChunkDataAndUpdateLight};
 mod finish_configuration;
-pub use finish_configuration::FinishConfigurationClientbound;
 mod keepalive;
-pub use keepalive::ClientboundKeelAlivePlay;
 mod login_success;
-pub use login_success::{LoginSuccess, LoginSuccessProperty};
 mod play;
-pub use play::{DeathInfo, Play};
 mod registry_data;
-pub use registry_data::RegistryData;
 mod set_default_spawn_position;
-pub use set_default_spawn_position::SetDefaultSpawnPosition;
 mod status_response;
-pub use status_response::StatusResponse;
 mod synchronize_player_position;
+
+pub use chunk_data_and_update_light::{BlockEntity, ChunkDataAndUpdateLight};
+pub use finish_configuration::FinishConfigurationClientbound;
+pub use keepalive::ClientboundKeelAlivePlay;
+pub use login_success::{LoginSuccess, LoginSuccessProperty};
+pub use play::{DeathInfo, Play};
+pub use registry_data::RegistryData;
+pub use set_default_spawn_position::SetDefaultSpawnPosition;
+pub use status_response::StatusResponse;
 pub use synchronize_player_position::SynchronizePlayerPosition;
