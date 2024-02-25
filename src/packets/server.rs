43d04c683ea8e45680154359d298f8c38e7f9e8c
ya7on <hello@syoma.website>
2024-02-25 16:14:21 +0300

    Sort packet exports

--- src/packets/server.rs
+++ src/packets/server.rs
@@ -1,18 +1,19 @@
 //! Packets that bound to server (from client to server)
 
 mod finish_configuration;
-pub use finish_configuration::FinishConfigurationServerbound;
 mod handshake;
-pub use handshake::{Handshake, HandshakeNextState};
 mod keepalive;
-pub use keepalive::ServerboundKeelAlivePlay;
 mod login_acknowledged;
-pub use login_acknowledged::LoginAcknowledged;
 mod login_start;
-pub use login_start::LoginStart;
 mod ping;
-pub use ping::PingRequest;
 mod set_player_position;
-pub use set_player_position::SetPlayerPosition;
 mod status_request;
+
+pub use finish_configuration::FinishConfigurationServerbound;
+pub use handshake::{Handshake, HandshakeNextState};
+pub use keepalive::ServerboundKeelAlivePlay;
+pub use login_acknowledged::LoginAcknowledged;
+pub use login_start::LoginStart;
+pub use ping::PingRequest;
+pub use set_player_position::SetPlayerPosition;
 pub use status_request::StatusRequest;
